044e0acf6159eb410f2a03f3d0cd4f6c86daedd3
bearc0025 <bcahill@etsy.com>
2024-07-15 11:02:35 -0500

    Initial Commit

---
+++ MobiusCounter/MobiusCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobiusCounterApp.swift
+//  MobiusCounter
+//
+//  Created by Bear Cahill on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MobiusCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
